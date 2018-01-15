de24c262e4781a456343e06ff44b7b889d1229c0
Joseph Wu <wuzhou88@gmail.com>
2018-01-15 20:16:32 +1100

    * .gitignore: intial commit
    * test.sln:
    * Main.cs:
    * App.xaml:
    * Info.plist:
    * test.shproj:
    * App.xaml.cs:
    * MainPage.xaml:
    * AppDelegate.cs:
    * test.iOS.csproj:
    * test.projitems:
    * Item.cs:
    * packages.config:
    * MainPage.xaml.cs:
    * packages.config:
    * MainActivity.cs:
    * MainPage.cs:
    * Entitlements.plist:
    * test.Droid.csproj:
    * AboutPage.xaml:
    * ItemsPage.xaml:
    * tab_feed.png:
    * tab_about.png:
    * NewItemPage.xaml:
    * IDataStore.cs:
    * LaunchScreen.storyboard:
    * AboutPage.xaml.cs:
    * ItemsPage.xaml.cs:
    * AboutAssets.txt:
    * tab_feed@3x.png:
    * tab_feed@2x.png:
    * xamarin_logo.png:
    * ItemDetailPage.xaml:
    * tab_about@3x.png:
    * MockDataStore.cs:
    * NewItemPage.xaml.cs:
    * tab_about@2x.png:
    * CloudDataStore.cs:
    * BaseViewModel.cs:
    * AssemblyInfo.cs:
    * ItemDetailPage.xaml.cs:
    * xamarin_logo@2x.png:
    * ItemsViewModel.cs:
    * Contents.json:
    * AboutViewModel.cs:
    * icon.png:
    * xamarin_logo@3x.png:
    * styles.xml:
    * Tabbar.axml:
    * AboutResources.txt:
    * Toolbar.axml:
    * Resource.designer.cs:
    * AndroidManifest.xml:
    * ItemDetailViewModel.cs:
    * icon.png:
    * icon.png:
    * icon.png:
    * xamarin_logo.png:
    * Contents.json:

---
+++ test/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace test
+{
+    public partial class MainPage : ContentPage
+    {
+        string translatedNumber;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnTranslate (object sender, EventArgs e){
+            // translatedNumber = Core
+        }
+
+    }
+}
